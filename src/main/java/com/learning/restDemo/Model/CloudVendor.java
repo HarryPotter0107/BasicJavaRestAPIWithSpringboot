293d549affbb6fe472ab7ef9adb88bbb1b30c268
HarryPotter0107 <bhanuupadhyay87@gmail.com>
2024-02-29 02:19:31 +0530

    DONE

---
+++ src/main/java/com/learning/restDemo/Model/CloudVendor.java
@@ -0,0 +1,53 @@
+package com.learning.restDemo.Model;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+public class CloudVendor {
+
+    private String vendorId;
+    private String VendorName;
+    private String vendorAddress;
+    private String vendorPhoneNumber;
+
+    public CloudVendor() {
+    }
+
+    public CloudVendor(String vendorId, String vendorName, String vendorAddress, String vendorPhoneNumber) {
+        this.vendorId = vendorId;
+        VendorName = vendorName;
+        this.vendorAddress = vendorAddress;
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+
+    public String getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(String vendorId) {
+        this.vendorId = vendorId;
+    }
+
+    public String getVendorName() {
+        return VendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        VendorName = vendorName;
+    }
+
+    public String getVendorAddress() {
+        return vendorAddress;
+    }
+
+    public void setVendorAddress(String vendorAddress) {
+        this.vendorAddress = vendorAddress;
+    }
+
+    public String getVendorPhoneNumber() {
+        return vendorPhoneNumber;
+    }
+
+    public void setVendorPhoneNumber(String vendorPhoneNumber) {
+        this.vendorPhoneNumber = vendorPhoneNumber;
+    }
+}
---
+++ src/test/java/com/learning/restDemo/RestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.restDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
